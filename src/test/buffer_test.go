242cc67e4bfee2348ae01373a5519d3093412404
Michal Strzelczyk <kontakt@digitalfarm.pl>
2019-10-09 23:15:15 +0200

    Unit test changes

--- src/test/buffer_test.go
+++ src/test/buffer_test.go
@@ -46,7 +46,6 @@ func TestIsEmpty(t *testing.T) {
 	}	
 }
 
-
 func TestUseNumber(t *testing.T) {
 	buffer := buffer.New()
 	
@@ -56,6 +55,41 @@ func TestUseNumber(t *testing.T) {
 	buffer.UseNumber(9)
 	
 	if buffer.List != [9]int8{0,2,3,4,0,0,7,8,0} {
-		t.Errorf("buffer.UseNumber not working properly. Buffer is empty")
+		t.Errorf("buffer.UseNumber not working properly. Buffer should look like 0,2,3,4,0,0,7,8,0")
+	}		
+}
+
+func TestGetRandomAvaiableNumberScenario1(t *testing.T) {
+	buffer := buffer.New()
+	
+	buffer.UseNumber(1)Available
+	buffer.UseNumber(2)
+	buffer.UseNumber(3)
+	buffer.UseNumber(4)
+	buffer.UseNumber(5)
+	buffer.UseNumber(6)
+	buffer.UseNumber(7)
+	buffer.UseNumber(8)
+	
+	if buffer.GetRandomAvaiableNumber() != 9 {
+		t.Errorf("buffer.GetRandomAvaiableNumber not working properly. Available number is 9")
+	}		
+}
+
+func TestGetRandomAvaiableNumberScenario2(t *testing.T) {
+	buffer := buffer.New()
+	
+	buffer.UseNumber(1)
+	buffer.UseNumber(2)
+
+	buffer.UseNumber(4)
+	buffer.UseNumber(5)
+	buffer.UseNumber(6)
+	buffer.UseNumber(7)
+	buffer.UseNumber(8)
+	
+	number := buffer.GetRandomAvaiableNumber()
+	if !(number == 3 || number == 9) {
+		t.Errorf("buffer.GetRandomAvaiableNumber not working properly. Shuffled number: %d Available: [3,9]", number)
 	}		
 }
